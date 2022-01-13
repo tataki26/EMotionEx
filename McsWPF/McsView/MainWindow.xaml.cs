8e0f58966e626cbf0bd8693505b37842fe576af8
tataki26 <tataki26@gmail.com>
2022-01-13 17:46:41 +0900

    Create McsView project and make connect button

---
+++ McsWPF/McsView/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace McsView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Button 컨트롤 클릭 이벤트 핸들러
+        private void cnnBtn_Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            MessageBox.Show("접속 완료!");
+        }
+    }
+}
