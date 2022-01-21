d2e26fdab4ed7b0f93ed4218e10e285d91f979bf
tataki26 <tataki26@gmail.com>
2022-01-21 18:09:40 +0900

    Refactor McsView project with binding data and adding dll files

--- McsWPF/McsView/MainWindow.xaml.cs
+++ McsWPF/McsView/MainWindow.xaml.cs
@@ -3,31 +3,23 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using McsView.ViewModel;
-using NetworkFrame01;
 using System.Windows.Threading;
+using VtpLib;
+using TdLib;
 
 namespace McsView
 {
     public partial class MainWindow : Window
     {
-        private VirtualTableProtocol vtp = new VirtualTableProtocol();
-        private ThreadingData td = new ThreadingData();
+        private MainViewModel mvm = new MainViewModel();
 
         public MainWindow()
         {
             InitializeComponent();
 
-            DispatcherTimer timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromTicks(1000000);
-            timer.Tick += new EventHandler(timerTick);
-            timer.Start();
-
             this.Loaded += OnLoaded;
-            this.DataContext = new MainViewModel();
+            this.DataContext = mvm;
         }
-
-        int addr;
-        int data;
 
         private void OnLoaded(object sender, RoutedEventArgs e)
         {
@@ -37,23 +29,16 @@ namespace McsView
             ipAddr.SelectedIndex = 0;
         }
 
-        private void timerTick(object sender, EventArgs e)
-        {
-            var viewModel = this.DataContext as MainViewModel;
-
-            if (td.lresult != null) viewModel.LprData = td.lresult.ToString();
-            if (td.iresult != null) viewModel.IpData = td.iresult.ToString();
-        }
-
         private void cnnBtn_Button_Click_1(object sender, RoutedEventArgs e)
         {
             string host = "192.168.240.2";
             // string host = "192.168.140.2"; // 연결 오류 확인용 코드
             int port = 2025;
 
-            vtp.Connect_Udp_Client(host, port);
-            td.Connect(host, port);
+            mvm.Connect(host, port);
         }
+
+        int addr, data;
 
         private void qpBtn_Button_Click(object sender, RoutedEventArgs e)
         {
@@ -66,7 +51,10 @@ namespace McsView
                 return;
             }
 
-            qpWrite();
+            int.TryParse(viewModel.QpAddr, out addr);
+            int.TryParse(viewModel.QpData, out data);
+
+            mvm.qpWrite(addr, data);
 
         }
 
@@ -81,63 +69,35 @@ namespace McsView
                 return;
             }
 
-            lpWrite();
-        }
-
-        private bool qpWrite()
-        {
-            var viewModel = this.DataContext as MainViewModel;
-
-            // MessageBox.Show(string.Format("Q접점: {0}", viewModel.QpAddr));
-            int.TryParse(viewModel.QpAddr, out addr);
-            int.TryParse(viewModel.QpData, out data);
-
-            vtp.Set_Q_Var(addr, data);
-
-            return true;
-        }
-
-        private bool lpWrite()
-        {
-            var viewModel = this.DataContext as MainViewModel;
-
-            // MessageBox.Show(string.Format("L접점: {0}", viewModel.LpwAddr));
             int.TryParse(viewModel.LpwAddr, out addr);
             int.TryParse(viewModel.LpwData, out data);
 
-            vtp.Set_LW_Var(addr, data);
-
-            return true;
+            mvm.lpWrite(addr, data);
         }
 
         private void lprAddr_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            var viewModel = this.DataContext as MainViewModel;
-
             string selectedDt = lprAddr.SelectedIndex.ToString();
+            
             int.TryParse(selectedDt, out addr);
 
-            td.lVarAddress = addr;
-
-            if (viewModel != null) viewModel.LprData = data.ToString();
+            mvm.lUpdatedAddr = addr;
 
         }
 
         private void ipAddr_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            var viewModel = this.DataContext as MainViewModel;
-
             string selectedDt = ipAddr.SelectedIndex.ToString();
+            
             int.TryParse(selectedDt, out addr);
 
-            td.lVarAddress = addr;
+            mvm.iUpdatedAddr = addr;
 
-            if (viewModel != null) viewModel.IpData = data.ToString();
         }
 
         private void dnnBtn_Button_Click(object sender, RoutedEventArgs e)
         {
-            td.Disconnect();
+            mvm.Disconnect();
             MessageBox.Show("접속 종료!");
         }
     }
--- McsWPF/McsView/ViewModel/MainViewModel.cs
+++ McsWPF/McsView/ViewModel/MainViewModel.cs
@@ -1,10 +1,24 @@
 ﻿using System.ComponentModel;
+using VtpLib;
+using System.Threading;
 
 namespace McsView.ViewModel
 {
     class MainViewModel : INotifyPropertyChanged
     {
-        private string _qpAddr;
+        private VirtualTableProtocol vtp = new VirtualTableProtocol();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _qpAddr, _lpwAddr, _lprAddr, _ipAddr;
+        private string _qpData, _lpwData;
+
+        public int lUpdatedAddr, iUpdatedAddr;
+        public object lUpdatedData, iUpdatedData;
+
+        Thread thread;
+        private bool flag = true;
+
         public string QpAddr
         {
             get { return _qpAddr; }
@@ -15,7 +29,6 @@ namespace McsView.ViewModel
             }
         }
 
-        private string _lpwAddr;
         public string LpwAddr
         {
             get { return _lpwAddr; }
@@ -26,7 +39,6 @@ namespace McsView.ViewModel
             }
         }
 
-        private string _lprAddr;
         public string LprAddr
         {
             get { return _lprAddr; }
@@ -37,7 +49,6 @@ namespace McsView.ViewModel
             }
         }
 
-        private string _ipAddr;
         public string IpAddr
         {
             get { return _ipAddr; }
@@ -48,7 +59,6 @@ namespace McsView.ViewModel
             }
         }
 
-        private string _qpData;
         public string QpData
         {
             get { return _qpData; }
@@ -59,7 +69,6 @@ namespace McsView.ViewModel
             }
         }
 
-        private string _lpwData;
         public string LpwData
         {
             get { return _lpwData; }
@@ -70,30 +79,70 @@ namespace McsView.ViewModel
             }
         }
 
-        private string _lprData;
         public string LprData
         {
-            get { return _lprData; }
+            get { return lUpdatedData?.ToString(); }
             set
             {
-                _lprData = value;
+                lUpdatedData = value;
                 OnPropertyUpdate("LprData");
             }
         }
 
-        private string _ipData;
         public string IpData
         {
-            get { return _ipData; }
+            get { return iUpdatedData?.ToString(); }
             set
             {
-                _ipData = value;
+                iUpdatedData = value;
                 OnPropertyUpdate("IpData");
             }
         }
 
+        public bool qpWrite(int addr, int data)
+        {
 
-        public event PropertyChangedEventHandler PropertyChanged;
+            vtp.Set_Q_Var(addr, data);
+
+            return true;
+        }
+
+        public bool lpWrite(int addr, int data)
+        {
+
+            vtp.Set_LW_Var(addr, data);
+
+            return true;
+        }
+
+        public void Connect(string host, int port)
+        {
+            vtp.Connect_Udp_Client(host, port);
+
+            thread = new Thread(new ThreadStart(ThreadGetData));
+            thread.Start();
+
+        }
+
+        public void Disconnect()
+        {
+            flag = false;
+            thread?.Join();
+        }
+
+        public void ThreadGetData()
+        {
+            // object tempObject = null;
+            while (flag)
+            {
+                vtp.Set_LR_Var(lUpdatedAddr, ref lUpdatedData);
+                LprData = lUpdatedData.ToString();
+
+                vtp.Set_I_Var(iUpdatedAddr, ref iUpdatedData);
+                IpData = iUpdatedData.ToString();
+
+            }
+        }
 
         private void OnPropertyUpdate(string propertyName)
         {
