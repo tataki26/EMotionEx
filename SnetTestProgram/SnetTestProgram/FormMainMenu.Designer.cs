3a8ec2edfd02fc2fe62846052c8f21ef5698617b
tataki26 <tataki26@gmail.com>
2022-02-09 15:27:56 +0900

    Add MenuBarPanel and update UI

--- SnetTestProgram/SnetTestProgram/FormMainMenu.Designer.cs
+++ SnetTestProgram/SnetTestProgram/FormMainMenu.Designer.cs
@@ -29,20 +29,126 @@ namespace SnetTestProgram
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMainMenu));
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnInterrupt = new System.Windows.Forms.Button();
+            this.panelTitleBar = new System.Windows.Forms.Panel();
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.panelMenu.SuspendLayout();
+            this.panelTitleBar.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panelMenu.Controls.Add(this.btnInterrupt);
+            this.panelMenu.Controls.Add(this.btnConnect);
+            this.panelMenu.Controls.Add(this.panelLogo);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(220, 581);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(180)))), ((int)(((byte)(255)))));
+            this.panelLogo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelLogo.BackgroundImage")));
+            this.panelLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(220, 80);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnConnect.FlatAppearance.BorderSize = 0;
+            this.btnConnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConnect.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.ForeColor = System.Drawing.Color.AliceBlue;
+            this.btnConnect.Image = ((System.Drawing.Image)(resources.GetObject("btnConnect.Image")));
+            this.btnConnect.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnConnect.Location = new System.Drawing.Point(0, 80);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnConnect.Size = new System.Drawing.Size(220, 60);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "   Connect";
+            this.btnConnect.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnConnect.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnInterrupt
+            // 
+            this.btnInterrupt.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnInterrupt.FlatAppearance.BorderSize = 0;
+            this.btnInterrupt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInterrupt.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInterrupt.ForeColor = System.Drawing.Color.AliceBlue;
+            this.btnInterrupt.Image = ((System.Drawing.Image)(resources.GetObject("btnInterrupt.Image")));
+            this.btnInterrupt.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnInterrupt.Location = new System.Drawing.Point(0, 140);
+            this.btnInterrupt.Name = "btnInterrupt";
+            this.btnInterrupt.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnInterrupt.Size = new System.Drawing.Size(220, 60);
+            this.btnInterrupt.TabIndex = 2;
+            this.btnInterrupt.Text = "   Interrupt";
+            this.btnInterrupt.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnInterrupt.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnInterrupt.UseVisualStyleBackColor = true;
+            this.btnInterrupt.Click += new System.EventHandler(this.btnInterrupt_Click);
+            // 
+            // panelTitleBar
+            // 
+            this.panelTitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(170)))), ((int)(((byte)(250)))));
+            this.panelTitleBar.Controls.Add(this.lbTitle);
+            this.panelTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitleBar.Location = new System.Drawing.Point(220, 0);
+            this.panelTitleBar.Name = "panelTitleBar";
+            this.panelTitleBar.Size = new System.Drawing.Size(864, 80);
+            this.panelTitleBar.TabIndex = 1;
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.Font = new System.Drawing.Font("Bahnschrift", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitle.ForeColor = System.Drawing.Color.White;
+            this.lbTitle.Location = new System.Drawing.Point(393, 27);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(87, 33);
+            this.lbTitle.TabIndex = 0;
+            this.lbTitle.Text = "HOME";
+            // 
+            // FormMainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(1084, 581);
+            this.Controls.Add(this.panelTitleBar);
+            this.Controls.Add(this.panelMenu);
+            this.Name = "FormMainMenu";
             this.Text = "Form1";
+            this.panelMenu.ResumeLayout(false);
+            this.panelTitleBar.ResumeLayout(false);
+            this.panelTitleBar.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Button btnInterrupt;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Panel panelTitleBar;
+        private System.Windows.Forms.Label lbTitle;
     }
 }
 
--- SnetTestProgram/SnetTestProgram/FormMainMenu.cs
+++ SnetTestProgram/SnetTestProgram/FormMainMenu.cs
@@ -12,9 +12,55 @@ namespace SnetTestProgram
 {
     public partial class FormMainMenu : Form
     {
+        #region Fields
+        private Button currentButton;
+        #endregion
+
         public FormMainMenu()
         {
             InitializeComponent();
         }
+
+        #region Methods
+        private void ActivateButton(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (currentButton != (Button)btnSender)
+                {
+                    DisableButton();
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = Color.FromArgb(140, 200, 255);
+                    currentButton.ForeColor = Color.White;
+                    currentButton.Font = new System.Drawing.Font("Bahnschrift", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                }
+            }
+        }
+
+        private void DisableButton()
+        {
+            foreach (Control previousBtn in panelMenu.Controls)
+            {
+                if (previousBtn.GetType() == typeof(Button))
+                {
+                    previousBtn.BackColor = Color.CornflowerBlue;
+                    previousBtn.ForeColor = Color.AliceBlue;
+                    previousBtn.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                }
+            }
+        }
+        #endregion
+
+        #region Events
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btnInterrupt_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+        #endregion
     }
 }
